bc148193182b2af271711cad2cdc4e098415ab47
NoyanTM <doge.channel@gmail.com>
2025-03-16 22:09:58 +0500

    fix: remake some parts from assignment 1

--- assignment_1/README.md
+++ assignment_1/README.md
@@ -8,15 +8,15 @@
 
 ## Solution
 
-1. Adding new section and changing source binary via lief library:
+1. Modifying entrypoint address of source binary, which probably will cause segmentation fault because programm will try to access memory not allowed to it or non-existing segment:
+```
+./script.sh
+```
+
+2. Adding new section and changing source binary via lief library:
 ```
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 python3 -m src.main
-```
-
-2. Modifying entrypoint address of source binary, which probably will cause segmentation fault because programm will try to access memory not allowed to it or non-existing segment:
-```
-./script.sh
 ```
--- assignment_1/src/main.py
+++ assignment_1/src/main.py
@@ -1,36 +1,33 @@
+import stat
+from pathlib import Path
+import subprocess
+
 import lief
 
 
-def main():
-    
-    with open("some_executable_2", "rb") as f:
-        parsed_binary = lief.ELF.parse(f)
-        
-    # binary_data = {
-    #     "elf_type": parsed_binary.header.file_type,
-    #     "architecture": parsed_binary.header.machine_type,
-    # }
-
-    rodata = parsed_binary.get_section(".rodata")
+def modify_rodata_section(binary_object: lief.Binary, base_directory: Path):
+    rodata = binary_object.get_section(".rodata")
     data = bytearray(rodata.content)
-
     old_string = b"Hello"
     new_string = b"Goodbye"
-
-
     offset = data.find(old_string)
     if offset == -1:
-        raise
-
+        raise Exception("Cannot find string in the source data")
     for i in range(len(new_string)):
         data[offset + i] = new_string[i]
-
-
     rodata.content = list(data)
+    modified_file = base_directory / "build/example_modified.o" 
+    binary_object.write(str(modified_file))
+    modified_file.chmod(modified_file.stat().st_mode | stat.S_IEXEC) # add chmod +x to the file
 
-    parsed_binary.write("hello_modified")
-    
-    # chmod +x
+
+def main():
+    BASE_DIR = Path(__file__).parent.parent
+    build_file_directory = BASE_DIR / "build/example.o"
+    subprocess.run(["gcc", "-o", f"{build_file_directory}", f"{BASE_DIR}/src/example.c"]) # recompile binary again
+    with build_file_directory.open("rb") as f:
+        parsed_binary = lief.ELF.parse(f)
+    modify_rodata_section(binary_object=parsed_binary, base_directory=BASE_DIR)
     
 
 if __name__ == "__main__":
